4247a40a6bbdd8078cc92b00e6e29fe3df4d23a5
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-25 17:49:28 +0100

    Aggiunti bonus

--- app.js
+++ app.js
@@ -19,34 +19,40 @@ const app = new Vue({
                 done: false
             },
         ]
-
+        
     },
-
+    
     methods: {
-
+        
         deleteTodo: function(i){
-
+            
             this.toDoList.splice(i,1);
-
+            
         },
-
+        
         addTodo: function(){
-
+            
             if(this.todo !== '') {
-
+                
                 const newTodo = {
                     text: this.todo,
                     done: false
                 }
-
+                
                 this.toDoList.push(newTodo);
-
+                
             }
-
+            
             this.todo = '';
+            
+        },
+        
+        doneInvert: function(i){
+            
+            this.toDoList[i].done = !this.toDoList[i].done;
 
         }
-
+        
     }
 
 });
