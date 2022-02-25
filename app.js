349b450bf6a98b97aa253575fc3647c7b8ed2bc9
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-25 15:53:23 +0100

    Aggiunto milestone 2,3

--- app.js
+++ app.js
@@ -3,27 +3,50 @@ const app = new Vue({
     el: '#app',
     data: {
 
+        todo: '',
+
         toDoList: [
             {
                 text: 'Fare la spesa',
-                done: false,
+                done: false
             },
             {
                 text: 'Comprare il latte',
-                done: true,
+                done: true
             },
             {
                 text: 'Fare esercizi',
-                done: false,
+                done: false
             },
-        ],
+        ]
 
     },
 
     methods: {
 
+        deleteTodo: function(i){
 
+            this.toDoList.splice(i,1);
+
+        },
+
+        addTodo: function(){
+
+            if(this.todo !== '') {
+
+                const newTodo = {
+                    text: this.todo,
+                    done: false
+                }
+
+                this.toDoList.push(newTodo);
+
+            }
+
+            this.todo = '';
+
+        }
 
     }
 
-})
+});
