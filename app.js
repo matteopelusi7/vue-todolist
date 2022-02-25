14e513c57ff950b8b7c9ebb25c0044d176cdc131
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-25 15:39:47 +0100

    Aggiunti item todo

---
+++ app.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+
+        toDoList: [
+            {
+                text: 'Fare la spesa',
+                done: false,
+            },
+            {
+                text: 'Comprare il latte',
+                done: true,
+            },
+            {
+                text: 'Fare esercizi',
+                done: false,
+            },
+        ],
+
+    },
+
+    methods: {
+
+
+
+    }
+
+})
